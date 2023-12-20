5e0a650eaaf2c0ad4f20b7b04e2d2d1d2f6b68a3
Rumanito12 <ady_9697@yahoo.com>
2023-12-20 21:40:48 +0100

    ADD: se añade metodo GET en la ruta para traer todos los eventos de la base de datos

---
+++ app/Http/Controllers/Events_Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\events_routes;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Laravel\Prompts\error;
+
+class Events_Controller extends Controller
+{
+    public function list_events(Request $request)
+    {
+        try {
+            if(auth()->user()->is_active === 0){
+                throw error('usuario borrado');
+            }
+            $events = events_routes::get(['*']);
+            return response()->json(
+                [
+                    'succes' => true,
+                    'message' => 'usuarios',
+                    'data' => $events
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'succes' => false,
+                    'message' => 'Error marking user as inactive',
+                    'error' => $th->getMessage()
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Events_Controller;
 use App\Http\Controllers\Users_Controller;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [Users_Controller::class, 'list_users']);
     Route::put('/user', [Users_Controller::class, 'update_user']);
     Route::delete('/user', [Users_Controller::class, 'delete_user']);
+});
+
+//Events
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/events', [Events_Controller::class, 'list_events']);
 });
