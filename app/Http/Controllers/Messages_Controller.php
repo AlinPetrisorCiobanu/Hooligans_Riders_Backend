fcb2802108156a3944a2d48d8f75ffe785ba6119
Rumanito12 <ady_9697@yahoo.com>
2024-01-06 15:57:38 +0100

    ADD: se añade metodo de crear nuevos mensajes tomando el id del remitente

--- app/Http/Controllers/Messages_Controller.php
+++ app/Http/Controllers/Messages_Controller.php
@@ -20,12 +20,12 @@ class Messages_Controller extends Controller
                 throw error('no tienes permiso');
             }
             $page_count = $request->query('count', 6);
-            $events = messages::paginate($page_count);
+            $messages = messages::where('is_active', 1)->paginate($page_count);
             return response()->json(
                 [
                     'succes' => true,
-                    'message' => 'usuarios',
-                    'data' => $events
+                    'message' => 'mensajes',
+                    'data' => $messages
                 ],
                 Response::HTTP_OK
             );
@@ -38,6 +38,49 @@ class Messages_Controller extends Controller
                 ],
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
-        } 
+        }
+    }
+
+    public function new_message(Request $request)
+    {
+        try {
+            if (auth()->user()->is_active === 0) {
+                throw error('usuario borrado');
+            }
+
+            //recoger info
+            $name = $request->input('name');
+            $last_name = $request->input('last_name');
+            $data = $request->input('data');
+            $message = $request->input('message');
+
+            $new_message = messages::create(
+                [
+                    'id_user' => auth()->user()->id,
+                    'name' => $name,
+                    'last_name' => $last_name,
+                    'data' => $data,
+                    'message' => $message,
+                ]
+            );
+
+            return response()->json(
+                [
+                    'succes' => true,
+                    'message' => 'mensaje',
+                    'data' => $new_message
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'succes' => false,
+                    'message' => 'Error marking user as inactive',
+                    'error' => $th->getMessage()
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 }
